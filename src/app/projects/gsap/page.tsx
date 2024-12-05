431a12b51991bd49c9892f5f5aa09ce4be763386
Priya Misner <priyamisner@Priyas-MacBook-Pro.local>
2024-12-05 11:31:15 -0500

    ref issue

--- src/app/projects/gsap/page.tsx
+++ src/app/projects/gsap/page.tsx
@@ -10,68 +10,53 @@ import {
 } from "./style/GSAPStyle";
 
 export default function GSAP() {
-  const container = useRef();
-  const jan = useRef();
-  const feb = useRef();
-  const mar = useRef();
-  const apr = useRef();
-  const may = useRef();
-  const jun = useRef();
-  const jul = useRef();
-  const aug = useRef();
-  const sept = useRef();
-  const oct = useRef();
-  const nov = useRef();
-  const dec = useRef();
+  const container = useRef(null);
+  const jan = useRef<HTMLDivElement>(null);
+  const feb = useRef<HTMLDivElement>(null);
+  const mar = useRef<HTMLDivElement>(null);
+  const apr = useRef<HTMLDivElement>(null);
+
   const circle = useRef();
 
   useGSAP(
     () => {
       let tl = gsap.timeline();
-
-      //  refs...
-      tl.to(jan?.current, {
-        y: "+=600",
-        // opacity: "-=0.8",
-        duration: 4,
-        color: "#012973",
-      });
-      tl.to(
-        feb?.current,
-        {
+      if (jan.current && feb.current && mar.current && apr.current) {
+        //  refs...
+        tl.to(jan?.current, {
           y: "+=600",
-          color: "#012973",
+          // opacity: "-=0.8",
           duration: 4,
-        },
-        "<0.5"
-      );
-      tl.to(
-        mar?.current,
-        {
-          y: "+=600",
           color: "#012973",
-          duration: 4,
-        },
-        "<0.5"
-      );
-      tl.to(
-        apr?.current,
-        {
-          y: "+=600",
-          color: "#012973",
-          duration: 4,
-        },
-        "<0.7"
-      );
-      tl.to(
-        may?.current,
-        {
-          y: "+=600",
-          color: "#012973",
-          duration: 4,
-        },
-        "<0.7"
-      );
+        });
+        tl.to(
+          feb?.current,
+          {
+            y: "+=600",
+            color: "#012973",
+            duration: 4,
+          },
+          "<0.5"
+        );
+        tl.to(
+          mar?.current,
+          {
+            y: "+=600",
+            color: "#012973",
+            duration: 4,
+          },
+          "<0.5"
+        );
+        tl.to(
+          apr?.current,
+          {
+            y: "+=600",
+            color: "#012973",
+            duration: 4,
+          },
+          "<0.7"
+        );
+      }
     },
     { scope: container }
   ); // <-- scope for selector text (optional)
@@ -83,14 +68,6 @@ export default function GSAP() {
         <SeasonDiv ref={feb}>February</SeasonDiv>
         <SeasonDiv ref={mar}>March</SeasonDiv>
         <SeasonDiv ref={apr}>April</SeasonDiv>
-        <SeasonDiv ref={may}>May</SeasonDiv>
-        <SeasonDiv ref={jun}>June</SeasonDiv>
-        <SeasonDiv ref={jul}>July</SeasonDiv>
-        <SeasonDiv ref={aug}>August</SeasonDiv>
-        <SeasonDiv ref={sept}>September</SeasonDiv>
-        <SeasonDiv ref={oct}>October</SeasonDiv>
-        <SeasonDiv ref={nov}>November</SeasonDiv>
-        <SeasonDiv ref={dec}>December</SeasonDiv>
       </Container>
     </PageWrapper>
   );
