c150d3c8facdcd6c8fd8da39ab7f02e204c7e86f
Priya Misner <priyamisner@Priyas-MacBook-Pro.local>
2025-12-07 18:07:45 -0800

    final:

--- src/app/projects/plane/style/style.tsx
+++ src/app/projects/plane/style/style.tsx
@@ -2,9 +2,84 @@
 import styled from "styled-components";
 
 export const PageWrapper = styled.div`
-  background-color: skyblue;
+  /* background-color: skyblue; */
   width: 100vw;
   height: 100vh;
+  background-color: #9db846;
+  background-image: url("/spriteSheets/grass3.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+export const Headline = styled.h1`
+  left: 24px;
+  top: 24px;
+  position: absolute;
+  max-width: 6vw;
+  font-size: clamp(3.5rem, 10vw, 6rem);
+  font-family: Georgia, "Times New Roman", Times, serif;
+  color: #141135;
+  opacity: 0.3;
+  filter: blur(3px);
+`;
+
+export const Subtitle = styled.h2`
+  right: 24px;
+  top: 32px;
+  position: absolute;
+  /* max-width: 6vw; */
+  font-size: 20px;
+  /* font-family: Georgia, "Times New Roman", Times, serif; */
+  color: #141135;
+  opacity: 0.3;
+  filter: blur(1.5px);
+  a,
+  &a:visited {
+    color: #141135;
+    text-decoration: underline;
+  }
+  @media (max-width: 576px) {
+    /* Styles applied ONLY when the screen width is 576px or narrower */
+    width: 20vw;
+  }
+`;
+
+export const Buttons = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(255, 255, 255, 0.1);
+  padding: 12px 12px 4px 12px;
+  border-radius: 5px 5px 0 0;
+  z-index: 1000; /* Ensure it stays on top of other content */
+  display: flex;
+  flex-direction: row;
+  column-gap: 16px;
+  border-radius: 12px 12px 0px 0px;
+`;
+
+interface ToggleButtonProps {
+  active: boolean;
+}
+export const Button = styled.button<ToggleButtonProps>`
+  background-color: ${(props) =>
+    props.active ? "rgba(255,255,255,0.7)" : "transparent"};
+  color: #494949;
+  text-align: center;
+  padding: 12px 12px 4px 12px;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: ${(props) => (props.active ? "0.9" : "1")};
+    background-color: ${(props) =>
+      props.active ? "rgba(255,255,255,0.7)" : "rgba(255,255,255,0.3)"};
+  }
+  svg {
+    width: 32px;
+  }
 `;
 
 export const PlanePlaceholder = styled.div`
@@ -28,7 +103,7 @@ export const PlanePlaceholder = styled.div`
 
 export const Flock = styled.div`
   /* background-color: olivedrab; */
-  opacity: 0.5;
+  opacity: 0.3;
   width: 250px;
   height: 150px;
   position: absolute;
